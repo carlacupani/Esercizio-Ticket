0e8dffd0f3c98f37ed992d061513d51bd0b1ce20
ghiro122 <125474001+ghiro122@users.noreply.github.com>
2023-12-11 12:48:22 +0100

    Update TicketService.java

--- backend/progTicket/src/main/java/com/ticket/service/TicketService.java
+++ backend/progTicket/src/main/java/com/ticket/service/TicketService.java
@@ -1,5 +1,26 @@
 package com.ticket.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
+
+import com.ticket.entity.Ticket;
+import com.ticket.repository.RepositoryTicket;
+
+@Service
 public interface TicketService {
 
+	
+	Iterable<Ticket> getTuttiTicket();
+
+	void save(Ticket ticket);
+
+	void updateTicket(Ticket ticket, Long id);
+
+	void deleteTicket(long id);
+	
+	
+	
 }
