d806f7e6792e75187c998db4583db51893713f40
Carla Cupani <cpnclp00@gmail.com>
2023-12-11 10:35:56 +0100

    commit

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { HomeAdminComponent } from './home-admin/home-admin.component';
 import { HomeUtenteComponent } from './home-utente/home-utente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     HomeAdminComponent,
     HomeUtenteComponent
   ],
