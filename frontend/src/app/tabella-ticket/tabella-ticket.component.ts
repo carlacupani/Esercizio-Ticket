4c6e809fb1da1739856642ffec14f2e54ef99e3f
ghiro122 <125474001+ghiro122@users.noreply.github.com>
2023-12-11 15:28:18 +0100

    Update tabella-ticket.component.ts

--- frontend/src/app/tabella-ticket/tabella-ticket.component.ts
+++ frontend/src/app/tabella-ticket/tabella-ticket.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { TicketserviceService } from '../ticketservice.service';
 
 @Component({
   selector: 'app-tabella-ticket',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./tabella-ticket.component.css']
 })
 export class TabellaTicketComponent {
+
+tickets: any;
+
+constructor(private ticketservice: TicketserviceService){
+
+}
+
+ngOnInit() {
+  console.log(this.tickets.descrizione)
+this.ticketservice.getAllTicket().subscribe(data=> {
+  this.tickets = data;
+})
+
+
+
+
+}
+modificaTicket()
+{
+
+}
+
+eliminaTicket() {
+
+}
+
 
 }
