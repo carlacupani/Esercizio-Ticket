a218baeb2dd9a8ea87fde7e212fc6f9b14e5d4b8
ghiro122 <125474001+ghiro122@users.noreply.github.com>
2023-12-11 12:48:39 +0100

    Update TicketServiceImpl.java

--- backend/progTicket/src/main/java/com/ticket/service/TicketServiceImpl.java
+++ backend/progTicket/src/main/java/com/ticket/service/TicketServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ticket.service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import com.ticket.entity.Ticket;
 import com.ticket.repository.RepositoryTicket;
@@ -12,6 +14,34 @@ public class TicketServiceImpl implements TicketService {
 	@Override
 	public Iterable<Ticket> getTuttiTicket() {
 		return repositoryTicket.findAll();
+	}
+
+	@Override
+	public void save(Ticket ticket) {
+		repositoryTicket.save(ticket);
+
+	}
+
+	@Override
+	public void updateTicket(Ticket ticket, Long id) {
+		Optional<Ticket> existingTicketOptional = repositoryTicket.findById(id);
+
+		// Estrai il ticket esistente
+		Ticket existingTicket = existingTicketOptional.get();
+
+		// Aggiorna i campi del ticket esistente con i dati forniti dal frontend
+		existingTicket.setDescrizione(ticket.getDescrizione()); // Sostituisci "Campo1" con il nome del campo
+		java.sql.Timestamp date = new java.sql.Timestamp(new java.util.Date().getTime());
+		existingTicket.setDate(date);
+
+		// Salva il ticket aggiornato nel database
+		repositoryTicket.save(existingTicket);
+
+	}
+
+	@Override
+	public void deleteTicket(long id) {
+		repositoryTicket.deleteById(id);
 	}
 
 }
